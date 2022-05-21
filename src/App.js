c052c4c5ab683857273ae93077d0368ead0fb893
Andrii Chuliy <44437096+chuliy@users.noreply.github.com>
2022-05-21 12:44:37 +0300

    routing and logo created

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import ListPage from './components/ListPage/ListPage';
+import EditPage from './components/EditPage/EditPage';
+import CreatePage from './components/CreatePage/CreatePage';
+
+function App() {
+  return (
+    <div>
+      <div className='logo'>Logo</div>
+      <div>
+        <div className='link-element'>
+          <Link to='/' className='links'>List Page</Link>
+        </div>
+        <div className='link-element'>
+          <Link to='/manageitems' className='links'>Manage Items</Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path='/' element={<ListPage/>}/>
+        <Route path='manageitems' element={<EditPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
