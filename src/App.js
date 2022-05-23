693c0921d24f0dcfa3cdbc7dfca9f2301859a20e
IvannaMelnyk <givmiller@gmail.com>
2022-05-23 13:51:00 +0300

    add searchBar to EditPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 import ListPage from './components/ListPage/ListPage';
 import EditPage from './components/EditPage/EditPage';
-import CreatePage from './components/CreatePage/CreatePage';
+
+// import CreatePage from './components/CreatePage/CreatePage';
 
 function App() {
   return (
     <div>
-      <div className='logo'>Logo</div>
+      <div className="logo">Logo</div>
       <div>
-        <div className='link-element'>
-          <Link to='/' className='links'>List Page</Link>
+        <div className="link-element">
+          <Link to="/" className="links">
+            List Page
+          </Link>
         </div>
-        <div className='link-element'>
-          <Link to='/manageitems' className='links'>Manage Items</Link>
+        <div className="link-element">
+          <Link to="/manageitems" className="links">
+            Manage Items
+          </Link>
         </div>
       </div>
       <Routes>
-        <Route path='/' element={<ListPage/>}/>
-        <Route path='manageitems' element={<EditPage/>}/>
+        <Route path="/" element={<ListPage />} />
+        <Route path="manageitems" element={<EditPage />} />
       </Routes>
     </div>
   );
