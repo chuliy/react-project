4e2fa4dae4b4b92bd313b6443e1d3614bc24ad4e
IvannaMelnyk <givmiller@gmail.com>
2022-06-08 11:43:05 +0300

    delete method in editPage

--- src/components/EditPageFilter/EditPageFilter.jsx
+++ src/components/EditPageFilter/EditPageFilter.jsx
@@ -1,35 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './EditPageFilter.module.css';
+// import EditableProduct from './EditableProduct';
 import { v4 as uuidv4 } from 'uuid';
-// import { getItems } from '../../serverqueries';
-// import { Items } from '../ListPage/Items/Items';
+import { getItems, getCategories, deleteItem } from '../../serverqueries';
+
 // This holds a list of some fiction products
 // Some  have the same name but different cost and id
-const PRODUCTS = [
-  { id: uuidv4(), categoryId: 1, name: 'Einaudi', cost: 500 },
-  { id: uuidv4(), categoryId: 2, name: 'Brahms', cost: 600 },
-  { id: uuidv4(), categoryId: 3, name: 'Tom Henk', cost: 700 },
-  { id: uuidv4(), categoryId: 4, name: 'Tom Handric', cost: 800 },
+const initialProducts = [
+  { id: uuidv4(), categoryId: 'tv', name: 'Einaudi', cost: 500 },
+  { id: uuidv4(), categoryId: 'tv', name: 'Brahms', cost: 600 },
+  { id: uuidv4(), categoryId: 'tv', name: 'Tom Henk', cost: 700 },
+  { id: uuidv4(), categoryId: 'tv', name: 'Tom Handric', cost: 800 },
 ];
 
-function App() {
+// function DemoComponent(props) {
+//   const { stateProducts } = props;
+//   if (!stateProducts || !stateProducts.length) {
+//     return <h1>Loading</h1>;
+//   }
+
+function EditPageFilter() {
   // the value of the search field
   const [name, setName] = useState('');
+  const [val] = useState({
+    name: '',
+    categoryId: null,
+    id: '',
+  });
+  const [stateProducts, setStateProducts] = useState([]);
+  const [stateCategories, setStateCategories] = useState([]);
 
+  function matchCategory(categoryId, categoriesListOfObj) {
+    const categoryObj = categoriesListOfObj.find(obj => obj.id === categoryId);
+    debugger;
+
+    return categoryObj.name;
+  }
+  useEffect(() => {
+    getItems().then(items => setStateProducts(items));
+    getCategories().then(items => setStateCategories(items));
+  }, []);
+  useEffect(() => {
+    deleteItem()
+      // async function dltItem(idToDelete, setStateProducts) {
+      //   await fetch(`${baseUrl}/product/${idToDelete}`, {
+      //     method: 'DELETE',
+      //     headers: { 'Content-Type': 'application/json' },
+      //     params: { id: idToDelete },
+      //     body: null,
+      //   })
+      // .then(res => {
+      //   getItems().then((res)=> {setState(res)})
+      // })
+      .then(() => getItems())
+      .then(items => setStateProducts(items))
+      .then(console.log(`Item deleted`));
+  }, []);
+  const deleteItems = idToDelete => {
+    setStateProducts(stateProducts.filter(item => item.id !== idToDelete));
+    console.log('delete from fetch');
+  };
+  let mappedStateToJsx = stateProducts.map(categoryObj => {
+    return (
+      <li key={categoryObj.id} className={s.product}>
+        <span className={s.id}>
+          {matchCategory(categoryObj.categoryId, stateCategories)}
+        </span>
+        <span className={s.name}>{categoryObj.name}</span>
+
+        <button type="submit" className={s.button}>
+          Edit
+        </button>
+        <button
+          type="submit"
+          className={s.buttonDelete}
+          value={val}
+          onClick={() => deleteItems(categoryObj.id)}
+        >
+          Delete
+        </button>
+      </li>
+    );
+  });
+
+  // let mappedStateToJsx = stateProducts.map(el => {});
   // the search result
-  const [foundProducts, setFoundProducts] = useState(PRODUCTS);
+  const [foundProducts, setFoundProducts] = useState(initialProducts);
 
   const filter = e => {
     const keyword = e.target.value;
 
     if (keyword !== '') {
-      const results = PRODUCTS.filter(product => {
+      const results = initialProducts.filter(product => {
         return product.name.toLowerCase().startsWith(keyword.toLowerCase());
         // Use the toLowerCase() method to make it case-insensitive
       });
       setFoundProducts(results);
     } else {
-      setFoundProducts(PRODUCTS);
+      setFoundProducts(initialProducts);
       // If the text field is empty, show all products
     }
 
@@ -49,10 +117,11 @@ function App() {
         className={s.input}
         placeholder="Search by name"
       />
+
       <div className={s.list}>
         {foundProducts && foundProducts.length > 0 ? (
           foundProducts.map(product => (
-            <li key={product.id} className={s.product}>
+            <li key={product.id} className={s.productInitial}>
               <span className={s.id}>{product.categoryId}</span>
               <span className={s.name}>{product.name}</span>
               {/* <span className={s.cost}>{product.cost}$</span> */}
@@ -72,10 +141,12 @@ function App() {
         ) : (
           <h1>No results found!</h1>
         )}
+        {mappedStateToJsx}
+        {/* <DemoComponent /> */}
+        {/* <EditableProduct onRemove={deleteContact} /> */}
       </div>
-      {/* <Items />; */}
     </div>
   );
 }
 
-export default App;
+export default EditPageFilter;
--- src/serverqueries.js
+++ src/serverqueries.js
@@ -1,46 +1,49 @@
+export const baseUrl = 'http://localhost:3000';
 // GET product list
 export async function getItems() {
-    let result = await fetch("http://localhost:3000/products")
-        .then(res => res.json());
-    return result
+  let result = await fetch(`${baseUrl}/products`).then(res => res.json());
+  return result;
 }
 
 // GET categories list
 export async function getCategories() {
-    let result = await fetch("http://localhost:3000/categories")
-        .then(res => res.json())
-    return result
+  let result = await fetch(`${baseUrl}/categories`).then(res => res.json());
+  return result;
 }
 
 // POST new item
 export async function postItem(item) {
-    await fetch('http://localhost:3000/product', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            name: item.name,
-            cost: item.cost,
-            categoryId: item.categoryId
-        })
-    })
+  await fetch(`${baseUrl}/product`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: item.name,
+      cost: item.cost,
+      categoryId: item.categoryId,
+    }),
+  });
 }
 
 // DELETE item
 export async function deleteItem(idToDelete) {
-    await fetch(`http://localhost:3000/product/${idToDelete}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-        params: { id: idToDelete },
-        body: null
-    });
+  await fetch(`${baseUrl}/product/${idToDelete}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    params: { id: idToDelete },
+    body: null,
+  });
 }
 
 // MODIFY item on server
 export async function modifyItem(item) {
-    await fetch(`http://localhost:3000/product/${item.id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        params: { id: item.id },
-        body: JSON.stringify({ name: item.name, cost: item.cost, categoryId: item.categoryId })
-    })
+  await fetch(`${baseUrl}/product/${item.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    params: { id: item.id },
+    body: JSON.stringify({
+      name: item.name,
+      cost: item.cost,
+      categoryId: item.categoryId,
+    }),
+  });
 }
