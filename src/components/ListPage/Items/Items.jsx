11c8f932bec9354ad1e4a7ac38d93a6263fe6cd9
naww <haha.yesitsme.1@gmail.com>
2022-06-08 11:43:41 +0300

    did some refactoring + added basic sorting on ListPage

--- src/components/ListPage/Items/Items.jsx
+++ src/components/ListPage/Items/Items.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { SortDropdown } from './SortDropdown/SortDropdown';
+import { Item } from './Item/Item';
 import "./Items.css";
 
-export const Items = (props) => {
+function matchCategory(categoryId, categoriesListOfObj) {
+    const category = categoriesListOfObj.find(obj => obj.id === categoryId);
+    return category.name
+}
 
-    return (
-        <div className='items-main-container'>
-            <SortDropdown pStateHandler={props.pStateHandler} pState={props.pState}/>
-            <div>
-                {props.pState.mappedItemsToJsx}
+export const Items = (props) => {
+    if (props.products.length > 0 && props.categories.length > 0) {
+        
+        const mappedItemsToJsx = props.products.map(
+            (el) =>
+            (<Item
+                key={Math.random()}
+                productName={el.name}
+                productCategoryId={matchCategory(el.categoryId, props.categories)}
+                productCost={el.cost}>
+            </Item>));
+        
+        return (
+            <div className='items-main-container'>
+                <SortDropdown parentStateHandler={props.parentStateHandler} products={props.products} />
+                <div>
+                    {mappedItemsToJsx}
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 };
--- src/components/ListPage/Items/SortDropdown/SortDropdown.jsx
+++ src/components/ListPage/Items/SortDropdown/SortDropdown.jsx
@@ -1,54 +1,70 @@
 import React, { useState } from 'react';
 import "./SortDropdown.css";
 
-function handleSorting(sortType, sortArr) {
+function sort(sortType, sortArr) {
+    // let tempVar = sortingFlag;
+    console.log("sort fucntion arguments:", sortType, sortArr);
     if (sortType === "by name") {
         sortArr.sort((a, b) => {
-            return a.name > b.name ? -1 : 1
+            return a.name > b.name ? 1 : -1
+        })
+    }
+    else if (sortType === "by price") {
+        sortArr.sort((a, b) => {
+            return Number(a.cost) > Number(b.cost) ? 1 : -1
+        })
+    }
+    else if (sortType === "by category") {
+        sortArr.sort((a, b) => {
+            return a.categoryId > b.categoryId ? 1 : -1 
         })
     }
     return sortArr
 }
 
-function liftSortResult(stateliftHandlerFn, sortedArr, state) {
-    stateliftHandlerFn({...state, products: sortedArr});
+function liftSortResult(stateliftHandlerFn, sortedArr) {
+    stateliftHandlerFn(sortedArr);
 }
 
 export const SortDropdown = (props) => {
     const sortTypes = ["by price", "by name", "by category"];
-    
+
     const [statea, setStatea] = useState({
         showDropdown: false,
         currentSortType: "Sort:"
     });
-    
-    const handleClick = (event) => {
-        setStatea({
-            showDropdown: !statea.showDropdown,
-            currentSortType: event.target.innerText
-        });
-        
-        if (event.target.innerText == "by name") {
-            let sortedArr = handleSorting(event.target.innerText, props.pState.products);
 
-            liftSortResult(props.pStateHandler, sortedArr, props.pState);
+    if (props.parentStateHandler) {
+        const handleClick = (event) => {
+            console.log("current PRE click state:", statea);
+            setStatea({
+                showDropdown: !statea.showDropdown,
+                currentSortType: event.target.innerText,
+            });
+            // ??? МОЖНО ЛИ СДЕЛАТЬ АДЕКВАТНЕЕ? -_-
+            let tempVar = [...props.products];
+            let sortedArr = sort(event.target.innerText, tempVar);
+
+            liftSortResult(props.parentStateHandler, sortedArr);
+            // liftSortResult(props.parentStateHandler, [{name: 'DNIWE', cost: '10001', categoryId: 1, id: '5xzqwe1q2134sd211'}, {name: 'SAMOE DNO', cost: '10001', categoryId: 1, id: '5xzqwe1q2134sd211'}])
         }
 
+        if (!statea.showDropdown) {
+            return (
+                <div className='dropdown-main-container'>
+                    <div className='d' onClick={handleClick}>{statea.currentSortType}</div>
+                </div >
+            );
+        } else {
+            const sortTypesHtml = sortTypes.map((el) => (<div className='d' key={el} onClick={handleClick}>{el}</div>))
+            return (
+                <div className='dropdown-main-container'>
+                    <div className='d' onClick={handleClick}>{statea.currentSortType}</div>
+                    {sortTypesHtml}
+                </div>
+            );
+        }
     }
 
-    if (!statea.showDropdown) {
-        return (
-            <div className='dropdown-main-container'>
-                <div className='d' onClick={handleClick}>{statea.currentSortType}</div>
-            </div >
-        );
-    } else {
-        const sortTypesHtml = sortTypes.map((el) => (<div className='d' key={el} onClick={handleClick}>{el}</div>))
-        return (
-            <div className='dropdown-main-container'>
-                <div className='d' onClick={handleClick}>{statea.currentSortType}</div>
-                {sortTypesHtml}
-            </div>
-        );
-    }
+
 };
