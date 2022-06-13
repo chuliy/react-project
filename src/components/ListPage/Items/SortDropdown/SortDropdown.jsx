3ba1cc322b6c1671ef6f9413d1f45f6b8927863d
naww <haha.yesitsme.1@gmail.com>
2022-06-13 21:11:16 +0300

    Added cypress, added sorting backwards, fixed sorting first row bug

--- src/components/ListPage/Items/SortDropdown/SortDropdown.jsx
+++ src/components/ListPage/Items/SortDropdown/SortDropdown.jsx
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
 import "./SortDropdown.css";
 
-function sort(sortType, sortArr) {
-    // let tempVar = sortingFlag;
+function sort(sortType, sortArr, sortFlags) {
     console.log("sort fucntion arguments:", sortType, sortArr);
-    if (sortType === "by name") {
+    if (sortType === "name") {
         sortArr.sort((a, b) => {
-            return a.name > b.name ? 1 : -1
+            return a.name > b.name ? -sortFlags.name : sortFlags.name
         })
     }
-    else if (sortType === "by price") {
+    else if (sortType === "price") {
         sortArr.sort((a, b) => {
-            return Number(a.cost) > Number(b.cost) ? 1 : -1
+            return Number(a.cost) > Number(b.cost) ? -sortFlags.price : sortFlags.price
         })
     }
-    else if (sortType === "by category") {
+    else if (sortType === "category") {
         sortArr.sort((a, b) => {
-            return a.categoryId > b.categoryId ? 1 : -1 
+            return a.categoryId > b.categoryId ? -sortFlags.category : sortFlags.category
         })
     }
     return sortArr
@@ -27,44 +26,80 @@ function liftSortResult(stateliftHandlerFn, sortedArr) {
 }
 
 export const SortDropdown = (props) => {
-    const sortTypes = ["by price", "by name", "by category"];
+    const sortTypes = ["price", "name", "category"];
 
-    const [statea, setStatea] = useState({
+    const [state, setState] = useState({
         showDropdown: false,
-        currentSortType: "Sort:"
+        currentSortType: "Sort by:",
+        sortFlags: {
+            price: -1,
+            name: -1,
+            category: -1
+        }
     });
 
     if (props.parentStateHandler) {
         const handleClick = (event) => {
-            console.log("current PRE click state:", statea);
-            setStatea({
-                showDropdown: !statea.showDropdown,
-                currentSortType: event.target.innerText,
-            });
-            // ??? МОЖНО ЛИ СДЕЛАТЬ АДЕКВАТНЕЕ? -_-
-            let tempVar = [...props.products];
-            let sortedArr = sort(event.target.innerText, tempVar);
+            if (event.target.innerText.includes("Sort by:")) {
+                setState({
+                    ...state,
+                    showDropdown: !state.showDropdown
+                });
+                
+            } else if (event.target.innerText.includes("price")) {
+                setState({
+                    showDropdown: !state.showDropdown,
+                    currentSortType: "Sort by:" + event.target.innerText,
+                    sortFlags: { ...state.sortFlags, price: -state.sortFlags.price }
+                });
 
-            liftSortResult(props.parentStateHandler, sortedArr);
-            // liftSortResult(props.parentStateHandler, [{name: 'DNIWE', cost: '10001', categoryId: 1, id: '5xzqwe1q2134sd211'}, {name: 'SAMOE DNO', cost: '10001', categoryId: 1, id: '5xzqwe1q2134sd211'}])
+                let tempVar = [...props.products];
+                let sortedArr = sort(event.target.innerText, tempVar, state.sortFlags);
+
+                liftSortResult(props.parentStateHandler, sortedArr);
+
+            } else if (event.target.innerText.includes("name")) {
+                setState({
+                    showDropdown: !state.showDropdown,
+                    currentSortType: "Sort by:" + event.target.innerText,
+                    sortFlags: { ...state.sortFlags, name: -state.sortFlags.name }
+                });
+
+                let tempVar = [...props.products];
+                let sortedArr = sort(event.target.innerText, tempVar, state.sortFlags);
+
+                liftSortResult(props.parentStateHandler, sortedArr);
+
+            } else if (event.target.innerText.includes("category")) {
+                setState({
+                    showDropdown: !state.showDropdown,
+                    currentSortType: "Sort by:" + event.target.innerText,
+                    sortFlags: { ...state.sortFlags, name: -state.sortFlags.category }
+                });
+
+                let tempVar = [...props.products];
+                let sortedArr = sort(event.target.innerText, tempVar, state.sortFlags);
+
+                liftSortResult(props.parentStateHandler, sortedArr);
+        
+            };
+
         }
 
-        if (!statea.showDropdown) {
-            return (
-                <div className='dropdown-main-container'>
-                    <div className='d' onClick={handleClick}>{statea.currentSortType}</div>
-                </div >
-            );
-        } else {
-            const sortTypesHtml = sortTypes.map((el) => (<div className='d' key={el} onClick={handleClick}>{el}</div>))
-            return (
-                <div className='dropdown-main-container'>
-                    <div className='d' onClick={handleClick}>{statea.currentSortType}</div>
-                    {sortTypesHtml}
-                </div>
-            );
+            if (!state.showDropdown) {
+                return (
+                    <div className='dropdown-main-container'>
+                        <div className='d' onClick={handleClick}>{state.currentSortType}</div>
+                    </div >
+                );
+            } else {
+                const sortTypesHtml = sortTypes.map((el) => (<div className='d' key={el} onClick={handleClick}>{el}</div>))
+                return (
+                    <div className='dropdown-main-container'>
+                        <div className='d' onClick={handleClick}>{state.currentSortType}</div>
+                        {sortTypesHtml}
+                    </div>
+                );
+            }
         }
-    }
-
-
-};
+    };
