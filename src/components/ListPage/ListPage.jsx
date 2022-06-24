0337d29a757e6a17f2480453364c0858b47d8015
naww <haha.yesitsme.1@gmail.com>
2022-06-24 03:05:00 +0300

    Added filters and made them work together with Sorting

--- src/components/ListPage/ListPage.jsx
+++ src/components/ListPage/ListPage.jsx
@@ -10,19 +10,18 @@ function initFilterState(categories) {
         var tmp = categories[i].name;
         obj[tmp] = false;
     }
-    console.log("INITIAL PRODUCT STATE", obj);
     return obj
 }
 
 const ListPage = (props) => {
 
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState({ products: [], backupProducts: [] });
     const [categories, setCategories] = useState([]);
     const [filterState, setFilterState] = useState({});
 
     useEffect(() => {
         getItems().then(res => {
-            setProducts(res);
+            setProducts({ products: res, backupProducts: res });
         })
         getCategories().then(res => {
             setCategories(res);
@@ -32,95 +31,48 @@ const ListPage = (props) => {
     }, []);
 
     const stateLiftHandler = (newState) => {
-        setProducts(newState);
+        setProducts({ products: newState, backupProducts: newState });
     }
 
     const handleFilters = (filterType, filterState) => {
-
         let tv1 = { ...filterState, [filterType]: !filterState[filterType] };
-        console.log("FILTER STATE:", tv1);
 
-        /// ????? как связать асинхронный setState с синхронным, который зависит от изменного этим setState'ом стейта?
+        /// ????? как связать асинхронный setState с синхронным кодом, который зависит от изменного этим setState'ом стейта?
         setFilterState({ ...tv1 });
         /// ?????
-        
-        if (tv1[filterType] == true) {
-            console.log("IF (TRUE)");
-            let arr = [];
-            let newState = [];
-            for (let key in tv1) {
-                if (tv1[key] === true) {
-                    for (let i = 0; i < categories.length; i++) {
-                        if (categories[i].name == key) {
-                            arr.push(categories[i].id);
-                        }
+
+        let arr = [];
+        let newState = [];
+        for (let key in tv1) {
+            if (tv1[key] === true) {
+                for (let i = 0; i < categories.length; i++) {
+                    if (categories[i].name == key) {
+                        arr.push(categories[i].id);
                     }
                 }
             }
-            console.log("ARRAY FOR NEW STATE: ", arr);
+        }
 
+        /// Как в addEventListener event передается не первым аргументом, если обычно закрепление аргумента идется по порядку?
 
-            /// ???????? как фильтровать по нескольким логическим условиям, если неизвестно кол-во этих условий?
-            newState = products.filter((obj) => { return (obj.categoryId == arr[0]) || (obj.categoryId == arr[1]) || (obj.categoryId == arr[2]) });
-            console.log("NEWSTATE:", newState);
-            /// ????????
-
-            setProducts(newState);
-            // setProducts([
-            //     {name: "TRUE",
-            //     categoryId: 1,
-            //     cost: "9999"},
-            //     {name: "TRUE1",
-            //     categoryId: 2,
-            //     cost: "1111"},
-            //     {name: "TRUE2",
-            //     categoryId: 3,
-            //     cost: "4444"}
-            // ]);
-            // regexpStr = regexpStr.slice(0, -1);
-            // let actualRegexp = new RegExp(`${regexpStr}`, 'i');
-            // console.log(actualRegexp, regexpStr);
-
-
-
-
-            // const res = () => {
-            //     const category = categories.find(obj => obj.name === filterType);
-            //     return category.id
-            // };
-            // const categoryId = res();
-
-            // let newState = productStateHolder.filter((obj) => { return obj.categoryId === categoryId });
-            // setProducts(newState);
+        /// ???????? как фильтровать по нескольким логическим условиям, если неизвестно кол-во этих условий?
+        newState = products.backupProducts.filter((obj) => { return (obj.categoryId == arr[0]) || (obj.categoryId == arr[1]) || (obj.categoryId == arr[2]) });
+        /// ????????
+        
+        if (newState.length > 0) {
+            setProducts({ ...products, products: newState });
         } else {
-            console.log("ELSE (FALSE)");
-
-            setProducts([
-                {
-                    name: "FALSE",
-                    categoryId: 1,
-                    cost: "9999"
-                },
-                {
-                    name: "FALSE1",
-                    categoryId: 2,
-                    cost: "1111"
-                },
-                {
-                    name: "FALSE2",
-                    categoryId: 3,
-                    cost: "4444"
-                }
-            ]);
+            setProducts({ ...products, products: products.backupProducts });
         }
     }
-
-    return (
-        <div className='list-page-main-container'>
-            <Filter filters={categories} filtersHandler={handleFilters} filterState={filterState} />
-            <Items products={products} categories={categories} parentStateHandler={stateLiftHandler} />
-        </div>
-    )
+    if (products.products) {
+        return (
+            <div className='list-page-main-container'>
+                <Filter filters={categories} filtersHandler={handleFilters} filterState={filterState} />
+                <Items products={products.products} categories={categories} parentStateHandler={stateLiftHandler} />
+            </div>
+        )
+    }
 };
 
 export default ListPage;
