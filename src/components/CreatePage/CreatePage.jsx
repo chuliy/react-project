85ae13a30190376be278318ecb1bd121fb81d4e2
naww <haha.yesitsme.1@gmail.com>
2022-07-07 12:26:56 +0300

    did some cleanup

--- src/components/CreatePage/CreatePage.jsx
+++ src/components/CreatePage/CreatePage.jsx
@@ -1,25 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "../CreatePage/CreatePage.css";
-import { getCategories, getItems, postItem } from '../../serverqueries';
+import { getCategories, postItem } from '../../serverqueries';
 import { useForm } from "react-hook-form";
 
 const CreatePage = () => {
   const [val, setVal] = useState({ name: "", categoryId: null, cost: "" });
   const [list, setList] = useState([]);
-  console.log(val);
 
   const { handleSubmit } = useForm();
   const onSubmit = (data, e) => { };
 
-
   useEffect(() => {
     getCategories().then(res => {
       setVal({ name: "", categoryId: res[0].id, cost: "" });
-      let tempVar = [];
-      for (let i = 0; i < res.length; i++) {
-        tempVar[i] = (<option key={Math.random()} value={res[i].id}>{res[i].name}</option>);
-      }
-      setList(tempVar);
+      const arr = res.map((el, i)=> el = (<option key={res[i].id} value={res[i].id}>{res[i].name}</option>))
+      setList(arr);
     });
   }, []);
 
@@ -35,7 +30,6 @@ const CreatePage = () => {
               className="input"
               type="text"
               data-cy="product-name"
-              // value={val.name} 
               onChange={(e) => setVal({ name: e.target.value, categoryId: val.categoryId, cost: val.cost })}
             />
           </div>
@@ -58,7 +52,6 @@ const CreatePage = () => {
               placeholder="Only numbers"
               className="input"
               data-cy="product-cost"
-              // value={val.cost}
               onChange={(e) => setVal({ name: val.name, categoryId: val.categoryId, cost: e.target.value })
               }
             />
@@ -67,16 +60,11 @@ const CreatePage = () => {
           <button className="add" id="cancel" onSubmit={onSubmit} >
             Cancel
           </button>
-          {/* <button className="add" onClick={() => {setVal({name:'',categoryId:null,cost:''})}}>
-            Reset
-          </button> */}
           <input className="add" type='reset'
             value="Save"
             data-cy="submit"
             onClick={(e) => {
               postItem({ name: val.name, categoryId: val.categoryId, cost: val.cost })
-              console.log('Item posted :', val)
-              console.log(getItems())
             }}
           >
           </input>
